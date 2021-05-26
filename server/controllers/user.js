ffdc3968591492312abe86835cf3e4b454aa411d
Joshua Leavitt <joshuadreyleavitt@gmail.com>
2021-05-25 20:14:15 -0600

    User Controller finished

---
+++ server/controllers/user.js
@@ -0,0 +1,60 @@
+const bcrypt = require('bcryptjs')
+
+module.exports = {
+    registerUser: async (req, res) => {
+        const db = req.app.get('db')
+        let { userName, password } = req.body
+
+        try {
+            const [foundUser] = await db.users.get_user(userName);
+            if (foundUser) {
+                res.status(401).send('Sorry, Account Already Exists');
+            } else {
+                const salt = bcrypt.genSaltSync(10)
+                const hash = bcrypt.HashSync(password, salt)
+                const [newUser] = await db.users.register_user([userName, hash])
+                delete newUser.password
+
+                req.session.user = newUser
+                req.status(200).send(req.session.user)
+            }
+        } catch (err) {
+            console.log('Database error on register function', err)
+        }
+    },
+
+
+    loginUser: async (req, res) => {
+        const db = req.app.get("db");
+        const { userName, password } = req.body;
+        console.log(req.body);
+        try {
+            const [foundUser] = await db.users.get_user(userName);
+            if (foundUser) {
+                const comparePassword = foundUser.password;
+                const authenticated = bcrypt.compareSync(password, comparePassword);
+                if (authenticated) {
+                    delete foundUser.password;
+                    req.session.user = foundUser;
+                    console.log(req.session.user);
+                    res.status(200).send(req.session.user);
+                } else {
+                    res.status(401).send("Incorrect username or password");
+                }
+            }
+        } catch (err) {
+            console.log("database error on login function", err);
+        }
+    },
+
+
+
+    logoutUser: (req, res) => {
+        req.session.destroy();
+        res.sendStatus(200)
+    },
+
+    getUser: (req, res) => {
+        res.status(200).send(req.session.user)
+    },
+}
--- server/index.js
+++ server/index.js
@@ -1,10 +1,40 @@
-const express = require('express');
+require("dotenv").config();
+const express = require("express");
+const massive = require("massive");
+const session = require("express-session");
+const { checkAdmin } = require("./controllers/middleware");
+const adminCtrl = require("./controllers/adminController");
+
+const { SERVER_PORT, CONNECTION_STRING, SESSION_SECRET } = process.env;
 
 const app = express();
 
 app.use(express.json());
 
+app.use(
+  session({
+    resave: false,
+    saveUninitialized: false,
+    secret: SESSION_SECRET,
+    cookie: {
+      maxAge: 1000 * 60 * 60 * 24,
+    },
+  })
+);
 
-const port = 3400;
+massive({
+  connectionString: CONNECTION_STRING,
+  ssl: {
+    rejectUnauthorized: false,
+  },
+})
+  .then((dbInstance) => {
+    app.set("db", dbInstance);
+    app.listen(SERVER_PORT, () =>
+      console.log(`Server & db is running ${SERVER_PORT}`)
+    );
+  })
+  .catch((err) => console.log(err));
 
-app.listen(port, () => {console.log(`Server is running on port ${port}`)});
+  //    ENDPOINTS - USER
+  
